ed967922b9df9f73fd776e8780c4a61ec7a9a1cb
Bjørn Dons <guardsmanpanda@gmail.com>
2022-05-14 10:28:45 +0200

    fix typo

--- src/Middleware/Initiate.php
+++ src/Middleware/Initiate.php
@@ -15,12 +15,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Initiate {
     public static array $headers =  ['X-Clacks-Overhead' => 'GNU Terry Pratchett'];
-    private Encrypter|null $encrypter = null;
+    private Encrypter|null $encrypt = null;
 
     public function __construct(private readonly Application $app) {
-        $key = Config::get('larabear.cookie.session_key');
+        $key = Config::get('bear.cookie.session_key');
         if ($key !== null) {
-            $this->encrypter = new Encrypter(key: base64_decode($key), cipher: Config::get('app.cipher'));
+            $this->encrypt = new Encrypter(key: base64_decode($key), cipher: Config::get('app.cipher'));
         }
     }
 
@@ -107,12 +107,12 @@ class Initiate {
             if ($name !== $session_name) {
                 throw new InvalidArgumentException('Cookie name must be the same as the session cookie.. name: ' . $name . ', session cookie: ' . $session_name);
             }
-            if ($this->encrypter === null) {
+            if ($this->encrypt === null) {
                 throw new InvalidArgumentException('Session cookie encryption key is not set, please run "php artisan bear:generate-session-key" to generate a key.');
             }
             $response->headers->setCookie(new Cookie(
                 name: $name,
-                value: $this->encrypter->encrypt(hash_hmac('sha256', $name, $this->encrypter->getKey()). '|' . $cookie->getValue()),
+                value: $this->encrypt->encrypt(hash_hmac('sha256', $name, $this->encrypt->getKey()). '|' . $cookie->getValue()),
                 expire: $cookie->getExpiresTime(),
                 path: '/',
                 secure: true,
@@ -130,8 +130,8 @@ class Initiate {
             if ($key !== $session_name) {
                 $request->cookies->remove($key);
             }
-            $value = $this->encrypter->decrypt($cookie);
-            $expected_prefix = hash_hmac('sha256', $key, $this->encrypter->getKey()) . '|';
+            $value = $this->encrypt->decrypt($cookie);
+            $expected_prefix = hash_hmac('sha256', $key, $this->encrypt->getKey()) . '|';
             if (!str_starts_with(haystack: $value, needle: $expected_prefix)) {
                 throw new InvalidArgumentException('Cookie value does not pass integrity check.. name: ' . $key);
             }
