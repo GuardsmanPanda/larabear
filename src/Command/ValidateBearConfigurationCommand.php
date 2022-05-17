1898f70c0a55566ec57aaafca5697c37cc926c47
Bjørn Dons <guardsmanpanda@gmail.com>
2022-05-17 15:21:34 +0200

    more checks

--- src/Command/ValidateBearConfigurationCommand.php
+++ src/Command/ValidateBearConfigurationCommand.php
@@ -19,10 +19,34 @@ class ValidateBearConfigurationCommand extends Command {
 
 
     private function validateSessionSettings(): void {
-        $this->terminalService->printH2(headline: 'Validate session settings');
+        $this->terminalService->printH2(headline: 'Validating session settings');
         $this->terminalService->printTestResult(
-            testName: 'Session Domain Should Be Null',
-            errorMessage: Config::get('session.domain') !== null ? "Session domain 'domain' in config/session.php should be null" : null
+            testName: 'Domain Should Be Null',
+            errorMessage: Config::get('session.domain') !== null ? "'domain'  in config/session.php should be null" : null
+        );
+        $this->terminalService->printTestResult(
+            testName: 'Http Only Should Be True',
+            errorMessage: Config::get('session.http_only') !== true ? "'http_only'  in config/session.php should be true" : null
+        );
+        $this->terminalService->printTestResult(
+            testName: 'Secure Should Be True',
+            errorMessage: Config::get('session.secure') !== true ? "'secure'  in config/session.php should be true" : null
+        );
+        $this->terminalService->printTestResult(
+            testName: "Path Should Be '/",
+            errorMessage: Config::get('session.path') !== '/' ? "'path'  in config/session.php should be '/'" : null
+        );
+        $this->terminalService->printTestResult(
+            testName: 'Same Site Should Be Strict or Lax',
+            errorMessage: in_array(needle:  Config::get('session.same_site'), haystack: ['lax', 'strict']) ? null : "'same_site'  in config/session.php should be null"
+        );
+        $this->terminalService->printTestResult(
+            testName: "Cookie Name Should Start With '__host-'",
+            errorMessage: str_starts_with(haystack: Config::get('session.cookie'), needle: '__host-') ? "'cookie'  in config/session.php should start_with '__host-'" : null
+        );
+        $this->terminalService->printTestResult(
+            testName: "Session Only Encryption Key Should Be Set",
+            errorMessage: Config::get('bear.cookie.session_key') !== null ? "Missing session encryption key, generate with 'php artisan bear:generate-session-key' and check 'cookie' => 'session_key' config/bear.php" : null
         );
     }
 }
--- src/Service/TerminalService.php
+++ src/Service/TerminalService.php
@@ -9,23 +9,19 @@ class TerminalService {
     public readonly int $terminalWidth;
 
     public function __construct(public readonly OutputStyle $output) {
-            $this->terminalWidth = max((new Terminal())->getWidth(), 30);
+        $this->terminalWidth = max((new Terminal())->getWidth(), 30);
     }
 
     public function printH1(string $headline): void {
-        $this->output->writeln("<fg=white;bg=blue;options=bold>$headline</>");
+        $this->output->writeln(str_pad("### <fg=green;options=bold>$headline</> ###" , $this->terminalWidth, ' ', STR_PAD_BOTH));
     }
 
     public function printH2(string $headline): void {
-        $this->output->writeln('');
-        $this->output->writeln(str_pad("  <fg=white;options=bold,underscore>$headline</>", $this->terminalWidth, ' ', STR_PAD_BOTH));
+        $this->output->writeln("<fg=blue;options=bold>$headline</>");
     }
 
     public function printTestResult(String $testName, string $errorMessage = null): void {
-        $prefix = $errorMessage === null ? "<bg=green;fg=black;options=bold>  PASS  </>" : "<bg=red;fg=black;options=bold>FAIL</>";
-        $this->output->writeln("    $prefix $testName");
-        if ($errorMessage !== null) {
-            $this->output->warning("    $errorMessage");
-        }
+        $prefix = $errorMessage === null ? "<bg=green;fg=black;options=bold>  PASS  </>" : "<bg=red;fg=white;options=bold>  FAIL  </>";
+        $this->output->writeln(" $prefix " . ($errorMessage ?? $testName));
     }
 }
