35349de1ea5200db399191deb9ec1bd74b23aed6
Bjørn Dons <guardsmanpanda@gmail.com>
2022-05-20 12:55:20 +0200

    Resp Fix.

--- src/Service/Resp.php
+++ src/Service/Resp.php
@@ -7,24 +7,12 @@ use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class Resp {
-    /**
-     * @param string $sql
-     * @param array<int, mixed> $data
-     * @return JsonResponse
-     */
     public static function SQLJson(string $sql, array $data = []): JsonResponse {
-        return new JsonResponse(DB::select("
-            SELECT json_agg(t) FROM ($sql) t
-        ", $data)[0]->json_agg ?? '[]', json: true);
+        return new JsonResponse(DB::select("SELECT json_agg(t) FROM ($sql) t", $data)[0]->json_agg ?? '[]', json: true);
     }
 
-    /**
-     * @param string $sql
-     * @param array<int, mixed> $data
-     * @return JsonResponse
-     */
     public static function SQLJsonSingle(string $sql, array $data = []): JsonResponse {
-        return new JsonResponse(DB::selectOne($sql, $data), json: true);
+        return new JsonResponse(DB::select(" SELECT row_to_json(t) FROM ($sql) t ", $data)[0]->row_to_json ?? '{}', json: true);
     }
 
     public static function header(string $name, string $value): void {
