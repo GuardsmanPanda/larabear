77f74c2e498de4cd7e362a269e7a8a21034da1e7
Bjørn Dons <guardsmanpanda@gmail.com>
2022-05-20 14:31:40 +0200

    session handling.

--- src/Middleware/SessionAuthMiddleware.php
+++ src/Middleware/SessionAuthMiddleware.php
@@ -3,20 +3,49 @@
 namespace GuardsmanPanda\Larabear\Middleware;
 
 use Closure;
+use DateTime;
 use Illuminate\Contracts\Session\Session;
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Http\Request;
 use Illuminate\Session\SessionManager;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ViewErrorBag;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 class SessionAuthMiddleware {
-    public function __construct(private readonly SessionManager $manager, private readonly ViewFactory $view) { }
+    private Session $session;
+    private array $config;
+    public function __construct(private readonly SessionManager $manager, private readonly ViewFactory $view) {
+        $this->config = Config::get('session');
+    }
 
     public function handle(Request $request, Closure $next) {
-        $session = $this->manager->driver()->setId($request->cookies->get(Config::get('session.cookie')));
-        $this->startSession($request, $session);
+        $this->session = $this->manager->driver()->setId($request->cookies->get(Config::get('session.cookie')));
+        $this->startSession($request, $this->session);
+
+        $response = $next($request);
+
+        $response->headers->setCookie(new Cookie(
+            name: $this->config['cookie'],
+            value: $this->session->getId(),
+            expire: $this->config['expire_on_close'] ? 0 : new DateTime('+' . $this->config['lifetime'] . ' minutes'),
+            path: $this->config['path'],
+            domain: $this->config['domain'],
+            secure: $this->config['secure'],
+            httpOnly: $this->config['http_only'],
+            sameSite: $this->config['same_site']
+        ));
+
+        $this->session->save();
         return $next($request);
+    }
+
+
+    public function terminate(Request $request, Response $response): void {
+        if (random_int(1, $this->config['lottery'][1]) <= $this->config['lottery'][0]) {
+            $this->session->getHandler()->gc($this->config['lifetime'] * 60);
+        }
     }
 
 
