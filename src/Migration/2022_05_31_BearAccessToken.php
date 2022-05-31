6a5f6b0477f08c9735972e8d2c2ce6ce4397af26
Bjørn Dons <guardsmanpanda@gmail.com>
2022-05-31 12:28:58 +0200

    fix migration issue

--- src/Migration/2022_05_31_BearAccessToken.php
+++ src/Migration/2022_05_31_BearAccessToken.php
@@ -14,7 +14,7 @@ return new class extends Migration {
                 $table->uuid(column: 'id')->primary()->default(DB::raw('uuid()'));
             }
             $table->uuid(column: 'id')->primary();
-            $table->text(column: 'api_route_prefix')->default(value: '');
+            $table->text(column: 'api_route_prefix')->default('');
             $table->ipAddress(column: 'ipv4_limit')->default('0.0.0.0/0');
             $table->ipAddress(column: 'ipv6_limit')->default('::/0');
             $table->timestampTz(column: 'expires_at')->nullable();
