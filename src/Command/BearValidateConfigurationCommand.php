538cead9c843fefec46dfe614ee0efc69cbc3b90
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-05 23:28:00 +0200

    config verify

--- src/Command/BearValidateConfigurationCommand.php
+++ src/Command/BearValidateConfigurationCommand.php
@@ -20,6 +20,7 @@ class BearValidateConfigurationCommand extends Command {
         }
         $this->validateSessionSettings();
         $this->validateUptimeKumaSettings();
+        $this->validateUserTableSettings();
         $this->output->writeln(messages: '');
     }
 
@@ -70,6 +71,36 @@ class BearValidateConfigurationCommand extends Command {
         $this->terminalService->printTestResult(
             testName: 'Uptime Kuma Should Be In Config',
             errorMessage: array_key_exists(key: 'base_url', array: $config) ? null : "Missing uptime kuma base url, add 'base_url' to 'uptime_kuma' in config/bear.php"
+        );
+    }
+
+    private function validateUserTableSettings(): void {
+        $this->terminalService->printH2(headline: 'Validating user table settings');
+        $config = Config::get(key: 'bear.user_table');
+        if ($config === null) {
+            $this->terminalService->printTestResult(
+                testName: 'User Table Settings Are Not Set',
+                warningMessage: 'Missing user table settings, add "user_table" to config/bear.php'
+            );
+            $this->output->writeln(messages: "
+    'user_table' => [
+        'table_name' => 'users',
+        'primary_key_column' => 'id',
+        'primary_key_type' => 'uuid', //Options uuid, integer, biginteger
+    ],");
+            return;
+        }
+        $this->terminalService->printTestResult(
+            testName: 'User Table Should Be In Config',
+            errorMessage: array_key_exists(key: 'table_name', array: $config) ? null : "Missing user table name, add 'table_name' to 'user_table' in config/bear.php"
+        );
+        $this->terminalService->printTestResult(
+            testName: 'Primary Key Column Should Be In Config',
+            errorMessage: array_key_exists(key: 'primary_key_column', array: $config) ? null : "Missing user table name, add 'primary_key_column' to 'user_table' in config/bear.php"
+        );
+        $this->terminalService->printTestResult(
+            testName: 'Primary Key Type Should Be In Config',
+            errorMessage: array_key_exists(key: 'primary_key_type', array: $config) ? null : "Missing user table name, add 'table_name' to 'primary_key_type' in config/bear.php"
         );
     }
 }
