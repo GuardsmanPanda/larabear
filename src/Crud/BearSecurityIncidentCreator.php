c134259aeb4192f1a521e9e1c7991207071fec81
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-07 09:56:49 +0200

    fix migration

--- src/Crud/BearSecurityIncidentCreator.php
+++ src/Crud/BearSecurityIncidentCreator.php
@@ -26,7 +26,7 @@ class BearSecurityIncidentCreator {
         }
         DB::insert("
             INSERT INTO bear_security_incident (security_incident_severity, security_incident_namespace, security_incident_headline, security_incident_description, security_incident_remediation, security_incident_reference, caused_by_user_id, request_ip, request_country_code, request_http_method, request_http_path, request_http_query)
-            VALUES (?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ", [$severity, $namespace, $headline, $description, $remediation, $reference, $causedByUserId, Req::ip(), Req::ipCountry(), Req::method(), Req::path(), $query_json]);
     }
 }
--- src/Migration/2022_05_31_BearSecurityIncident.php
+++ src/Migration/2022_05_31_BearSecurityIncident.php
@@ -15,26 +15,46 @@ return new class extends Migration {
 
         Schema::create(table: 'bear_security_incident', callback: static function (Blueprint $table) use ($config): void {
             $table->id();
-            $table->text(column: 'security_incident_severity');
-            $table->text(column: 'security_incident_namespace')->default('default');
-            $table->text(column: 'security_incident_headline');
-            $table->text(column: 'security_incident_description');
+
+            if (DB::getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'pgsql') {
+                $table->text(column: 'security_incident_severity');
+                $table->text(column: 'security_incident_namespace')->default('default');
+                $table->text(column: 'security_incident_headline');
+                $table->text(column: 'security_incident_description');
+            } else {
+                $table->string(column: 'security_incident_severity');
+                $table->string(column: 'security_incident_namespace')->default('default');
+                $table->string(column: 'security_incident_headline');
+                $table->string(column: 'security_incident_description');
+            }
+
             $table->text(column: 'security_incident_remediation')->nullable();
             $table->text(column: 'security_incident_reference')->nullable();
+
             if ($config['primary_key_type'] === 'uuid') {
                 $table->uuid(column: 'caused_by_user_id')->nullable();
             } else if ($config['primary_key_type'] === 'biginteger') {
                 $table->bigInteger(column: 'caused_by_user_id')->unsigned()->nullable();
-            }else if ($config['primary_key_type'] === 'integer') {
+            } else if ($config['primary_key_type'] === 'integer') {
                 $table->integer(column: 'caused_by_user_id')->unsigned()->nullable();
             } else {
                 $table->text(column: 'caused_by_user_id')->nullable();
             }
+
             $table->ipAddress(column: 'request_ip')->nullable();
-            $table->text(column: 'request_country_code')->nullable();
-            $table->text(column: 'request_http_method')->nullable();
-            $table->text(column: 'request_http_path')->nullable();
-            $table->text(column: 'request_http_query')->nullable();
+
+            if (DB::getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'pgsql') {
+                $table->text(column: 'request_country_code')->nullable();
+                $table->text(column: 'request_http_method')->nullable();
+                $table->text(column: 'request_http_path')->nullable();
+                $table->text(column: 'request_http_query')->nullable();
+            } else {
+                $table->string(column: 'request_country_code')->nullable();
+                $table->string(column: 'request_http_method')->nullable();
+                $table->string(column: 'request_http_path')->nullable();
+                $table->string(column: 'request_http_query')->nullable();
+            }
+
             $table->timestampTz(column: 'created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->foreign('caused_by_user_id')->references($config['primary_key_column'])->on($config['table_name']);
         });
