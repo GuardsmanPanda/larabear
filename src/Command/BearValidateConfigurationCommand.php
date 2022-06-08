503cccd69a65f80cb3b550a1d1438549df7a8e33
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-08 14:25:49 +0200

    Fix terminal service

--- src/Command/BearValidateConfigurationCommand.php
+++ src/Command/BearValidateConfigurationCommand.php
@@ -9,13 +9,11 @@ use Illuminate\Support\Facades\Config;
 class BearValidateConfigurationCommand extends Command {
     protected $signature = 'bear';
     protected $description = 'Validate the configuration for larabear';
-    private TerminalService $terminalService;
 
     public function handle(): void {
-        $this->terminalService = new TerminalService($this->output);
-        $this->terminalService->printH1(headline: 'Validating larabear configuration');
+        TerminalService::printH1(headline: 'Validating larabear configuration');
         if (Config::get(key: 'bear') === null) {
-            $this->terminalService->printTestResult(testName: '', errorMessage: 'Missing bear.php file in config/ directory please run "php artisan vendor:publish --tag=bear"');
+            TerminalService::printTestResult(testName: '', errorMessage: 'Missing bear.php file in config/ directory please run "php artisan vendor:publish --tag=bear"');
             return;
         }
         $this->validateSessionSettings();
@@ -26,32 +24,32 @@ class BearValidateConfigurationCommand extends Command {
 
 
     private function validateSessionSettings(): void {
-        $this->terminalService->printH2(headline: 'Validating session settings');
-        $this->terminalService->printTestResult(
+        TerminalService::printH2(headline: 'Validating session settings');
+        TerminalService::printTestResult(
             testName: 'Domain Should Be Null',
             errorMessage: Config::get('session.domain') !== null ? "'domain'  in config/session.php should be null .. Current setting: " . Config::get('session.domain') : null
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'Http Only Should Be True',
             errorMessage: Config::get('session.http_only') !== true ? "'http_only'  in config/session.php should be true .. Current setting: " . Config::get('session.http_only') : null
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'Secure Should Be True',
             errorMessage: Config::get('session.secure') !== true ? "'secure'  in config/session.php should be true .. Current setting: " . Config::get('session.secure') : null
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: "Path Should Be '/",
             errorMessage: Config::get('session.path') !== '/' ? "'path'  in config/session.php should be '/' .. Current setting: " . Config::get('session.path') : null
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'Same Site Should Be Strict or Lax',
             errorMessage: in_array(needle: Config::get('session.same_site'), haystack: ['lax', 'strict']) ? null : "'same_site'  in config/session.php should be null .. Current setting: " . Config::get('session.same_site')
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: "Cookie Name Should Start With '__host-'",
             errorMessage: str_starts_with(haystack: Config::get('session.cookie'), needle: '__host-') ? null : "'cookie'  in config/session.php should start_with '__host-' .. Current setting: " . Config::get('session.cookie')
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: "Session Only Encryption Key Should Be Set",
             errorMessage: Config::get('bear.cookie.session_key') === null ? "Missing session encryption key, generate with 'php artisan bear:generate-session-key' and check 'cookie' => 'session_key' config/bear.php" : null
         );
@@ -59,26 +57,26 @@ class BearValidateConfigurationCommand extends Command {
 
 
     private function validateUptimeKumaSettings(): void {
-        $this->terminalService->printH2(headline: 'Validating uptime kuma settings');
+        TerminalService::printH2(headline: 'Validating uptime kuma settings');
         $config = Config::get('bear.uptime_kuma');
         if ($config === null) {
-            $this->terminalService->printTestResult(
+            TerminalService::printTestResult(
                 testName: 'Uptime Kuma Settings Are Not Set',
                 warningMessage: 'Missing uptime kuma settings, add "uptime_kuma" to config/bear.php'
             );
             return;
         }
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'Uptime Kuma Should Be In Config',
             errorMessage: array_key_exists(key: 'base_url', array: $config) ? null : "Missing uptime kuma base url, add 'base_url' to 'uptime_kuma' in config/bear.php"
         );
     }
 
     private function validateUserTableSettings(): void {
-        $this->terminalService->printH2(headline: 'Validating user table settings');
+        TerminalService::printH2(headline: 'Validating user table settings');
         $config = Config::get(key: 'bear.user_table');
         if ($config === null) {
-            $this->terminalService->printTestResult(
+            TerminalService::printTestResult(
                 testName: 'User Table Settings Are Not Set',
                 errorMessage: 'Missing user table settings, add "user_table" to config/bear.php'
             );
@@ -90,15 +88,15 @@ class BearValidateConfigurationCommand extends Command {
     ],");
             return;
         }
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'User Table Should Be In Config',
             errorMessage: array_key_exists(key: 'table_name', array: $config) ? null : "Missing user table name, add 'table_name' to 'user_table' in config/bear.php"
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'Primary Key Column Should Be In Config',
             errorMessage: array_key_exists(key: 'primary_key_column', array: $config) ? null : "Missing user table name, add 'primary_key_column' to 'user_table' in config/bear.php"
         );
-        $this->terminalService->printTestResult(
+        TerminalService::printTestResult(
             testName: 'Primary Key Type Should Be In Config',
             errorMessage: array_key_exists(key: 'primary_key_type', array: $config) ? null : "Missing user table name, add 'table_name' to 'primary_key_type' in config/bear.php"
         );
--- src/Service/TerminalService.php
+++ src/Service/TerminalService.php
@@ -2,31 +2,39 @@
 
 namespace GuardsmanPanda\Larabear\Service;
 
-use Illuminate\Console\OutputStyle;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class TerminalService {
-    public function __construct(public readonly OutputStyle $output) {}
+    private static ConsoleOutput|null $output = null;
 
-    public function printH1(string $headline): void {
+    public static function printH1(string $headline): void {
         $around = str_pad(string: '', length: 120, pad_string: '#', pad_type: STR_PAD_BOTH);
         $headline = "  <fg=bright-green;options=bold>$headline</>  ";
-        $this->output->writeln(messages: $around);
-        $this->output->writeln(messages: str_pad(string: $headline, length: 120 + 33, pad_string: '#', pad_type: STR_PAD_BOTH));
-        $this->output->writeln(messages: $around);
+        self::writeLine(message: $around);
+        self::writeLine(message: str_pad(string: $headline, length: 120 + 33, pad_string: '#', pad_type: STR_PAD_BOTH));
+        self::writeLine(message: $around);
     }
 
-    public function printH2(string $headline): void {
-        $this->output->writeln(messages: '');
-        $this->output->writeln(messages: "<fg=bright-blue;options=bold>$headline</>");
+    public static function printH2(string $headline): void {
+        self::writeLine(message: '');
+        self::writeLine(message: "<fg=bright-blue;options=bold>$headline</>");
     }
 
-    public function printTestResult(string $testName, string $warningMessage = null, string $errorMessage = null): void {
+    public static function printTestResult(string $testName, string $warningMessage = null, string $errorMessage = null): void {
         $prefix = "<bg=green;fg=bright-green;options=bold>  PASS  </>";
         if ($errorMessage !== null) {
             $prefix = "<bg=bright-red;fg=white;options=bold>  FAIL  </>";
         } else if ($warningMessage !== null) {
             $prefix = "<bg=bright-yellow;fg=black;options=bold>  WARN  </>";
         }
-        $this->output->writeln(messages: " $prefix " . ($errorMessage ?? $warningMessage ?? $testName));
+        self::writeLine(message: " $prefix " . ($errorMessage ?? $warningMessage ?? $testName));
+    }
+
+
+    public static function writeLine(string $message): void {
+        if (self::$output === null) {
+            self::$output = new ConsoleOutput();
+        }
+        self::$output->writeln(messages: $message);
     }
 }
