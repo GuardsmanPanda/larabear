3de7e8c05a21af0ce0cd917e00511dff74c4402d
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-10 15:30:07 +0200

    fix query data

--- src/Infrastructure/Security/Crud/BearSecurityIncidentCreator.php
+++ src/Infrastructure/Security/Crud/BearSecurityIncidentCreator.php
@@ -27,6 +27,6 @@ class BearSecurityIncidentCreator {
         DB::insert(query: "
             INSERT INTO bear_security_incident (security_incident_severity, security_incident_namespace, security_incident_headline, security_incident_description, security_incident_remediation, security_incident_reference, caused_by_user_id, request_ip, request_country_code, request_http_method, request_http_path, request_http_query, request_http_hostname)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ", bindings: [$severity, $namespace, $headline, $description, $remediation, $reference, $causedByUserId, Req::ip(), Req::ipCountry(), Req::method(), Req::path(), $query_json, Req::hostname()]);
+        ", bindings: [$severity->value, $namespace, $headline, $description, $remediation, $reference, $causedByUserId, Req::ip(), Req::ipCountry(), Req::method(), Req::path(), $query_json, Req::hostname()]);
     }
 }
