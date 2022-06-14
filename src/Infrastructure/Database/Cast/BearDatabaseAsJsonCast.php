9767e12ab19626ce054635d155140b130d510233
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-14 14:26:45 +0200

    Add Json to Std Class Cast

---
+++ src/Infrastructure/Database/Cast/BearDatabaseAsJsonCast.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GuardsmanPanda\Larabear\Infrastructure\Database\Cast;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use stdClass;
+
+class BearDatabaseAsJsonCast implements CastsAttributes {
+    public function get($model, string $key, mixed $value, array$attributes): stdClass|null {
+        return $value === null ? new stdClass() : json_decode($value, false, 512, JSON_THROW_ON_ERROR);
+    }
+
+    public function set($model, string $key, mixed $value, array $attributes): string {
+        return json_encode($value, JSON_THROW_ON_ERROR);
+    }
+}
