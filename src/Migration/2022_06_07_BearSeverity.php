6d599b31fec74391b8c24b5a3947db0584d9623e
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-15 15:08:39 +0200

    migration fix

--- src/Migration/2022_06_07_BearSeverity.php
+++ src/Migration/2022_06_07_BearSeverity.php
@@ -17,16 +17,17 @@ return new class extends Migration {
                 $table->string(column: 'severity_name');
                 $table->string(column: 'severity_description');
             }
+            $table->tinyInteger(column: 'severity_level');
             $table->timestampTz(column: 'created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
 
         DB::table(table: 'bear_severity')->insert([
-            ['slug' => 'BASELINE', 'severity_name' => 'Baseline', 'severity_description' => 'Can safely be ignored, but should eventually be resolved.'],
-            ['slug' => 'LOW', 'severity_name' => 'Low', 'severity_description' => 'Unlikely to cause significant issues, but should be investigated.'],
-            ['slug' => 'MEDIUM', 'severity_name' => 'Medium', 'severity_description' => 'Might cause significant issues, or may have already caused minor issues.'],
-            ['slug' => 'HIGH', 'severity_name' => 'High', 'severity_description' => 'Likely to cause significant issues, or may have already cause moderate issues.'],
-            ['slug' => 'CRITICAL', 'severity_name' => 'Critical', 'severity_description' => 'Something is critically broken, MUST be investigated.'],
-            ['slug' => 'EMERGENCY', 'severity_name' => 'Emergency', 'severity_description' => 'All hands situation, MUST be resolved immediately.'],
+            ['slug' => 'BASELINE', 'severity_name' => 'Baseline', 'severity_description' => 'Can safely be ignored, but should eventually be resolved.', 'severity_level' => 6],
+            ['slug' => 'LOW', 'severity_name' => 'Low', 'severity_description' => 'Unlikely to cause significant issues, but should be investigated.', 'severity_level' => 5],
+            ['slug' => 'MEDIUM', 'severity_name' => 'Medium', 'severity_description' => 'Might cause significant issues, or may have already caused minor issues.', 'severity_level' => 4],
+            ['slug' => 'HIGH', 'severity_name' => 'High', 'severity_description' => 'Likely to cause significant issues, or may have already cause moderate issues.', 'severity_level' => 3],
+            ['slug' => 'CRITICAL', 'severity_name' => 'Critical', 'severity_description' => 'Something is critically broken, MUST be investigated.', 'severity_level' => 2],
+            ['slug' => 'EMERGENCY', 'severity_name' => 'Emergency', 'severity_description' => 'All hands situation, MUST be resolved immediately.', 'severity_level' => 1],
         ]);
 
         Schema::table(table: 'bear_security_incident', callback: static function (Blueprint $table): void {
--- src/Migration/2022_06_08_bear_idempotency.php
+++ src/Migration/2022_06_08_bear_idempotency.php
@@ -1,20 +1,13 @@
 <?php
 
-use GuardsmanPanda\Larabear\Infrastructure\Database\Service\BearMigrationService;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        $config = Config::get(key: 'bear.user_table');
-        if ($config === null) {
-            throw new RuntimeException(message: 'bear.user_table is not configured, run "php artisan bear" to fix this problem.');
-        }
-
-        Schema::create(table: 'bear_idempotency', callback: static function (Blueprint $table) use ($config): void {
+        Schema::create(table: 'bear_idempotency', callback: static function (Blueprint $table): void {
             if (DB::getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'pgsql') {
                 $table->text(column: 'idempotency_key')->primary();
                 $table->ipAddress(column: 'request_ip')->nullable();
