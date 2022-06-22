f0597fcab0a06ab8c23b7789f0daaab5993a98ef
Bjørn Dons <guardsmanpanda@gmail.com>
2022-06-22 11:58:16 +0200

    fix table name

--- src/Infrastructure/Security/Crud/BearSecurityIncidentCreator.php
+++ src/Infrastructure/Security/Crud/BearSecurityIncidentCreator.php
@@ -26,7 +26,7 @@ class BearSecurityIncidentCreator {
             Log::error(message: 'Failed to encode query parameters: ' . $e->getMessage());
         }
         DB::insert(query: "
-            INSERT INTO bear_security_incident (security_incident_severity, security_incident_namespace, security_incident_headline, security_incident_description, security_incident_remediation, security_incident_reference, caused_by_user_id, request_ip, request_country_code, request_http_method, request_http_path, request_http_query, request_http_hostname)
+            INSERT INTO bear_log_security_incident (security_incident_severity, security_incident_namespace, security_incident_headline, security_incident_description, security_incident_remediation, security_incident_reference, caused_by_user_id, request_ip, request_country_code, request_http_method, request_http_path, request_http_query, request_http_hostname)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ", bindings: [$severity->value, $namespace, $headline, $description, $remediation, $reference, $causedByUserId, Req::ip(), Req::ipCountry(), Req::method(), Req::path(), $query_json, Req::hostname()]);
     }
