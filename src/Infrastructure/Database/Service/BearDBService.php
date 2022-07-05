e411c9a080fe401ccf98e5840f06be2e3036ac35
Bjørn Dons <guardsmanpanda@gmail.com>
2022-07-05 20:39:30 +0200

    default DB connections driver.

--- src/Infrastructure/Database/Service/BearDBService.php
+++ src/Infrastructure/Database/Service/BearDBService.php
@@ -2,14 +2,19 @@
 
 namespace GuardsmanPanda\Larabear\Infrastructure\Database\Service;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use RuntimeException;
 
 class BearDBService {
     public static function mustBeInTransaction(): void {
-        if (DB::transactionLevel() !== 0) {
+        if (DB::transactionLevel() > 0) {
            return;
         }
         throw new RuntimeException(message: 'DB::transaction() must be called before calling this method.');
+    }
+
+    public static function defaultConnectionDriver(): string {
+        return Config::get(key: 'database.connections.'. Config::get(key: 'database.default') . '.driver');
     }
 }
