2e412daf2a8951dade895d732fb9836735e13123
Sander Hagenaars <sander.ha@hotmail.com>
2022-08-31 08:41:48 +0200

    fix wrong return type

--- src/Infrastructure/Database/Cast/BearAsJsonCast.php
+++ src/Infrastructure/Database/Cast/BearAsJsonCast.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use stdClass;
 
 class BearAsJsonCast implements CastsAttributes {
-    public function get($model, string $key, mixed $value, array$attributes): stdClass|null {
+    public function get($model, string $key, mixed $value, array$attributes): stdClass|null|array {
         return $value === null ? new stdClass() : json_decode($value, false, 512, JSON_THROW_ON_ERROR);
     }
 
