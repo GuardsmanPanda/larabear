cb8b5f8053b6c62d3e7ef60360e981b1677b875a
Bjørn Dons <guardsmanpanda@gmail.com>
2022-09-06 14:55:25 +0200

    add to service provider

--- src/Provider/BearServiceProvider.php
+++ src/Provider/BearServiceProvider.php
@@ -3,6 +3,7 @@
 namespace GuardsmanPanda\Larabear\Provider;
 
 use GuardsmanPanda\Larabear\Infrastructure\App\Command\BearValidateConfigurationCommand;
+use GuardsmanPanda\Larabear\Infrastructure\Database\Command\BearCheckForeignKeysOnSoftDeletesCommand;
 use GuardsmanPanda\Larabear\Infrastructure\Http\Command\BearGenerateSessionKeyCommand;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,6 +13,7 @@ class BearServiceProvider extends ServiceProvider {
             $this->commands(commands: [
                 BearGenerateSessionKeyCommand::class,
                 BearValidateConfigurationCommand::class,
+                BearCheckForeignKeysOnSoftDeletesCommand::class,
             ]);
             $this->publishes(paths: [__DIR__ . '/../../config/config.php' => $this->app->configPath(path: 'bear.php'),], groups: 'bear');
             $this->loadMigrationsFrom(paths: [ __DIR__.'/../Migration']);
