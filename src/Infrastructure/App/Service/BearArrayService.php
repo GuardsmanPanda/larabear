739dc22870eec384fb168b1734c0a285d6901d95
Bjørn Dons <guardsmanpanda@gmail.com>
2022-09-15 11:42:26 +0200

    fix

--- src/Infrastructure/App/Service/BearArrayService.php
+++ src/Infrastructure/App/Service/BearArrayService.php
@@ -6,7 +6,11 @@ class BearArrayService {
     public static function groupArrayBy(array $array, string $key): array {
         $result = [];
         foreach ($array as $item) {
-            $result[$item[$key]][] = $item;
+            if (is_array($item)) {
+                $result[$item[$key]][] = $item;
+            } else {
+                $result[$item->$key][] = $item;
+            }
         }
         return $result;
     }
