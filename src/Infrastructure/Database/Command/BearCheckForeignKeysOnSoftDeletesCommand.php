376b83c1d6365e372defb853bbe82398555dd225
Bjørn Dons <guardsmanpanda@gmail.com>
2022-09-20 13:03:55 +0200

    refactor db code.

--- src/Infrastructure/Database/Command/BearCheckForeignKeysOnSoftDeletesCommand.php
+++ src/Infrastructure/Database/Command/BearCheckForeignKeysOnSoftDeletesCommand.php
@@ -4,6 +4,7 @@ namespace GuardsmanPanda\Larabear\Infrastructure\Database\Command;
 
 use GuardsmanPanda\Larabear\Infrastructure\Console\Service\ConsoleService;
 use GuardsmanPanda\Larabear\Infrastructure\Database\Service\DatabaseBaseInformation;
+use GuardsmanPanda\Larabear\Infrastructure\Database\Service\DatabaseForeignKeyService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -14,34 +15,8 @@ class BearCheckForeignKeysOnSoftDeletesCommand extends Command {
     public function handle(): void {
         $defaultConnection = config(key: 'database.default');
         ConsoleService::printH1(headline: "Checking [$defaultConnection] Foreign Keys Against Soft Deletes");
-        $dbInfo = DatabaseBaseInformation::getInstance(connectionName: $defaultConnection);
-        $table_names = $dbInfo->getAllTableNames();
-        $tables_with_soft_deletes = [];
-
-        foreach ($table_names as $table_name) {
-            $columns = $dbInfo->getColumnsForTable(tableName: $table_name);
-            foreach ($columns as $column) {
-                if ($column->columnName === 'deleted_at') {
-                    $tables_with_soft_deletes[] = $table_name;
-                }
-            }
-        }
-        $foreign_keys = $dbInfo->getAllForeignKeys();
-        ConsoleService::printH2(headline: "Checking " . count($foreign_keys) . " Foreign Keys");
-        foreach ($foreign_keys as $foreign_key) {
-            if (!in_array($foreign_key->foreign_table, $tables_with_soft_deletes, true)) {
-                continue;
-            }
-            $where = in_array($foreign_key->table_name, $tables_with_soft_deletes, true) ? "AND t1.deleted_at IS NULL" : '';
-            $res = DB::connection($defaultConnection)->select("
-                SELECT
-                    * 
-                FROM $foreign_key->table_name t1
-                LEFT JOIN $foreign_key->foreign_table t2 ON t1.$foreign_key->column_name = t2.$foreign_key->foreign_key
-                WHERE t2.deleted_at IS NOT NULL $where
-                LIMIT 100
-            ");
-            $this->info(count($res) . " rows found in $foreign_key->table_name with a foreign key to $foreign_key->foreign_table that has been soft deleted");
-        }
+        $res = DatabaseForeignKeyService::getSoftDeletedForeignKeyConstraints(connection: $defaultConnection);
+        dd($res);
+        //$this->info(count($res) . " rows found in $foreign_key->table_name with a foreign key to $foreign_key->foreign_table that has been soft deleted");
     }
 }
---
+++ src/Infrastructure/Database/Service/DatabaseForeignKeyService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace GuardsmanPanda\Larabear\Infrastructure\Database\Service;
+
+use Illuminate\Support\Facades\DB;
+
+class DatabaseForeignKeyService {
+    public static function getSoftDeletedForeignKeyConstraints(string $connection): array {
+        $dbInfo = DatabaseBaseInformation::getInstance(connectionName: $connection);
+        $table_names = $dbInfo->getAllTableNames();
+        $tables_with_soft_deletes = [];
+
+        foreach ($table_names as $table_name) {
+            $columns = $dbInfo->getColumnsForTable(tableName: $table_name);
+            foreach ($columns as $column) {
+                if ($column->columnName === 'deleted_at') {
+                    $tables_with_soft_deletes[] = $table_name;
+                }
+            }
+        }
+
+        $foreign_keys = $dbInfo->getAllForeignKeys();
+        $result = [];
+        foreach ($foreign_keys as $foreign_key) {
+            if (!in_array($foreign_key->foreign_table, $tables_with_soft_deletes, true)) {
+                continue;
+            }
+            $where = in_array($foreign_key->table_name, $tables_with_soft_deletes, true) ? "AND t1.deleted_at IS NULL" : '';
+            $res = DB::connection($connection)->select("
+                SELECT
+                    * 
+                FROM $foreign_key->table_name t1
+                LEFT JOIN $foreign_key->foreign_table t2 ON t1.$foreign_key->column_name = t2.$foreign_key->foreign_key
+                WHERE t2.deleted_at IS NOT NULL $where
+                LIMIT 100
+            ");
+            if (count($res) > 0) {
+                $result[] = [
+                    'table_name' => $foreign_key->table_name,
+                    'column_name' => $foreign_key->column_name,
+                    'foreign_table' => $foreign_key->foreign_table,
+                    'foreign_key' => $foreign_key->foreign_key,
+                    'errors' => $res
+                ];
+            }
+        }
+        return $result;
+    }
+}
