7930efb8e0f28a248a67f9ef3dd58d6e6524b0b4
Bjørn Dons <guardsmanpanda@gmail.com>
2022-09-30 14:33:25 +0200

    fix

--- src/Infrastructure/Integrity/Crud/BearIdempotencyCreator.php
+++ src/Infrastructure/Integrity/Crud/BearIdempotencyCreator.php
@@ -2,14 +2,15 @@
 
 namespace GuardsmanPanda\Larabear\Infrastructure\Integrity\Crud;
 
+use GuardsmanPanda\Larabear\Infrastructure\App\Service\BearGlobalStateService;
 use GuardsmanPanda\Larabear\Infrastructure\Http\Service\Req;
 use Illuminate\Support\Facades\DB;
 
 class BearIdempotencyCreator {
     public static function create(string $idempotency_key): void {
         DB::insert(query: "
-            INSERT INTO bear_log_idempotency (idempotency_key, request_ip, request_country_code, request_http_method, request_http_path)
-            VALUES (?, ?, ?, ?, ?)
-        ", bindings: [$idempotency_key, Req::ip(), Req::ipCountry(), Req::method(), Req::path()]);
+            INSERT INTO bear_log_idempotency (idempotency_key, request_ip, request_country_code, request_http_method, request_http_path, request_id, console_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        ", bindings: [$idempotency_key, Req::ip(), Req::ipCountry(), Req::method(), Req::path(), BearGlobalStateService::getRequestId(), BearGlobalStateService::getConsoleId()]);
     }
 }
