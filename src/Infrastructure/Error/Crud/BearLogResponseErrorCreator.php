fa6952e10d5050cb5f277ca7c85d0e12bb46b5a1
Bjørn Dons <guardsmanpanda@gmail.com>
2022-10-03 10:18:55 +0200

    fix

--- src/Infrastructure/Error/Crud/BearLogResponseErrorCreator.php
+++ src/Infrastructure/Error/Crud/BearLogResponseErrorCreator.php
@@ -19,7 +19,7 @@ class BearLogResponseErrorCreator {
             DB::insert("
             INSERT INTO bear_log_response_error
             (request_ip, user_id, request_country_code, response_status_code, request_http_method, request_http_path, request_http_query_json, request_http_hostname, app_action_name, response_body, request_id)
-            VALUES (?, ?, ?, ? ,? ,? ,? ,? ,?, ?)",
+            VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?)",
                 [Req::ip(), BearGlobalStateService::getUserId(), Req::ipCountry(), $statusCode, Req::method(), Req::path(), $query_json, Req::hostname(), Req::actionName(), $responseBody, BearGlobalStateService::getRequestId()]);
         } catch (Throwable $e) {
             Log::error(message: 'Failed log error response: ' . $e->getMessage());
