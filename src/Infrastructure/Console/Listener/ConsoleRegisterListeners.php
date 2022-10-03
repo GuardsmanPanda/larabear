8fa8b7a49cb59a1f40103a55ffd8431c3f7c7ea7
Bjørn Dons <guardsmanpanda@gmail.com>
2022-10-03 12:12:27 +0200

    fix schedule call

---
+++ src/Infrastructure/Console/Listener/ConsoleRegisterListeners.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace GuardsmanPanda\Larabear\Infrastructure\Console\Listener;
+
+use Carbon\Carbon;
+use GuardsmanPanda\Larabear\Enum\BearSeverityEnum;
+use GuardsmanPanda\Larabear\Infrastructure\App\Service\BearGlobalStateService;
+use GuardsmanPanda\Larabear\Infrastructure\Console\Crud\BearLogConsoleEventCreator;
+use GuardsmanPanda\Larabear\Infrastructure\Console\Crud\BearLogConsoleEventUpdater;
+use GuardsmanPanda\Larabear\Infrastructure\Error\Crud\BearLogErrorCreator;
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Console\Events\ScheduledTaskFinished;
+use Illuminate\Console\Events\ScheduledTaskStarting;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Str;
+use Throwable;
+
+class ConsoleRegisterListeners {
+    private static array $ignoreCommands = [
+        '',
+        'about',
+        'migrate',
+        'optimize',
+        'package:discover',
+        'schedule:run',
+    ];
+
+    public static function RegisterListeners(): void {
+        Event::listen(events: CommandStarting::class, listener: static function ($event) {
+            if (in_array(needle: $event->command, haystack: self::$ignoreCommands, strict: true)) {
+                return;
+            }
+            BearGlobalStateService::setConsoleId(consoleId: Str::uuid()->toString());
+            try {
+                DB::beginTransaction();
+                BearLogConsoleEventCreator::create(
+                    console_event_type: 'command',
+                    console_command: $event->input->__toString(),
+                );
+                DB::commit();
+            } catch (Throwable $t) {
+                DB::rollBack();
+                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-command-starting', severity: BearSeverityEnum::MEDIUM, exception: $t);
+            }
+        });
+
+
+        Event::listen(events: ScheduledTaskStarting::class, listener: static function ($event) {
+            BearGlobalStateService::setConsoleId(consoleId: Str::uuid()->toString());
+            try {
+                DB::beginTransaction();
+                BearLogConsoleEventCreator::create(
+                    console_event_type: 'scheduled_task',
+                    console_command: $event->task->command ?? $event->task->getSummaryForDisplay(),
+                    cron_schedule_expression: $event->task->expression,
+                    cron_schedule_timezone: $event->task->timezone,
+                );
+                DB::commit();
+            } catch (Throwable $t) {
+                DB::rollBack();
+                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-scheduled-task-starting', severity: BearSeverityEnum::MEDIUM, exception: $t);
+            }
+        });
+
+
+        Event::listen(events: CommandFinished::class, listener: static function ($event) {
+            if (in_array(needle: $event->command, haystack: self::$ignoreCommands, strict: true)) {
+                return;
+            }
+            try {
+                DB::beginTransaction();
+                $updater = BearLogConsoleEventUpdater::fromConsoleEventId(consoleEventId: BearGlobalStateService::getConsoleId());
+                if ($event->exitCode === 0) {
+                    $updater->setConsoleEventFinishedAt(Carbon::now());
+                } else {
+                    $updater->setConsoleEventFailedAt(Carbon::now());
+                }
+                $updater->setExecutionTimeMicroseconds((int)((microtime(as_float: true) - get_defined_constants()['LARAVEL_START']) * 1_000_000));
+                $updater->save();
+                DB::commit();
+            } catch (Throwable $t) {
+                DB::rollBack();
+                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-command-finished', severity: BearSeverityEnum::MEDIUM, exception: $t);
+            }
+        });
+
+
+        Event::listen(events: ScheduledTaskFinished::class, listener: static function ($event) {
+            try {
+                DB::beginTransaction();
+                $updater = BearLogConsoleEventUpdater::fromConsoleEventId(consoleEventId: BearGlobalStateService::getConsoleId());
+                if ($event->task->exitCode === 0) {
+                    $updater->setConsoleEventFinishedAt(Carbon::now());
+                } else {
+                    $updater->setConsoleEventFailedAt(Carbon::now());
+                    $command = $event->task->command;
+                    BearLogErrorCreator::create(
+                        message: "Scheduled task [$command] failed",
+                        namespace: 'larabear',
+                        key: 'scheduled-task-failed',
+                        severity: BearSeverityEnum::HIGH,
+                    );
+                }
+                $updater->setExecutionTimeMicroseconds((int)($event->runtime * 1_000_000));
+                $updater->save();
+                DB::commit();
+            } catch (Throwable $t) {
+                DB::rollBack();
+                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-scheduled-task-finished', severity: BearSeverityEnum::MEDIUM, exception: $t);
+            }
+        });
+    }
+}
--- src/Provider/BearServiceProvider.php
+++ src/Provider/BearServiceProvider.php
@@ -2,39 +2,17 @@
 
 namespace GuardsmanPanda\Larabear\Provider;
 
-use Carbon\Carbon;
-use GuardsmanPanda\Larabear\Enum\BearSeverityEnum;
 use GuardsmanPanda\Larabear\Infrastructure\App\Command\BearValidateConfigurationCommand;
-use GuardsmanPanda\Larabear\Infrastructure\App\Service\BearGlobalStateService;
-use GuardsmanPanda\Larabear\Infrastructure\Console\Crud\BearLogConsoleEventCreator;
-use GuardsmanPanda\Larabear\Infrastructure\Console\Crud\BearLogConsoleEventUpdater;
+use GuardsmanPanda\Larabear\Infrastructure\Console\Listener\ConsoleRegisterListeners;
 use GuardsmanPanda\Larabear\Infrastructure\Database\Command\BearCheckForeignKeysOnSoftDeletesCommand;
-use GuardsmanPanda\Larabear\Infrastructure\Error\Crud\BearLogErrorCreator;
 use GuardsmanPanda\Larabear\Infrastructure\Http\Command\BearGenerateSessionKeyCommand;
-use Illuminate\Console\Events\CommandFinished;
-use Illuminate\Console\Events\CommandStarting;
-use Illuminate\Console\Events\ScheduledTaskFinished;
-use Illuminate\Console\Events\ScheduledTaskStarting;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
-use Throwable;
 
 class BearServiceProvider extends ServiceProvider {
-    private array $ignoreCommands = [
-        '',
-        'about',
-        'migrate',
-        'optimize',
-        'package:discover',
-        'schedule:run',
-    ];
-
 
     public function boot(): void {
         if ($this->app->runningInConsole()) {
-            $this->addEventListeners();
+            ConsoleRegisterListeners::RegisterListeners();
 
             $this->commands(commands: [
                 BearGenerateSessionKeyCommand::class,
@@ -45,92 +23,5 @@ class BearServiceProvider extends ServiceProvider {
             $this->publishes(paths: [__DIR__ . '/../../config/config.php' => $this->app->configPath(path: 'bear.php'),], groups: 'bear');
             $this->loadMigrationsFrom(paths: [__DIR__ . '/../Migration']);
         }
-    }
-
-
-    private function addEventListeners(): void {
-        Event::listen(events: CommandStarting::class, listener: function ($event) {
-            if (in_array($event->command, $this->ignoreCommands, true)) {
-                return;
-            }
-            BearGlobalStateService::setConsoleId(consoleId: Str::uuid()->toString());
-            try {
-                DB::beginTransaction();
-                BearLogConsoleEventCreator::create(
-                    console_event_type: 'command',
-                    console_command: $event->input->__toString(),
-                );
-                DB::commit();
-            } catch (Throwable $t) {
-                DB::rollBack();
-                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-command-starting', severity: BearSeverityEnum::MEDIUM, exception: $t);
-            }
-        });
-
-
-        Event::listen(events: ScheduledTaskStarting::class, listener: static function ($event) {
-            BearGlobalStateService::setConsoleId(consoleId: Str::uuid()->toString());
-            try {
-                DB::beginTransaction();
-                BearLogConsoleEventCreator::create(
-                    console_event_type: 'scheduled_task',
-                    console_command: $event->task->command,
-                    cron_schedule_expression: $event->task->expression,
-                    cron_schedule_timezone: $event->task->timezone,
-                );
-                DB::commit();
-            } catch (Throwable $t) {
-                DB::rollBack();
-                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-scheduled-task-starting', severity: BearSeverityEnum::MEDIUM, exception: $t);
-            }
-        });
-
-
-        Event::listen(events: CommandFinished::class, listener: function ($event) {
-            if (in_array($event->command, $this->ignoreCommands, true)) {
-                return;
-            }
-            try {
-                DB::beginTransaction();
-                $updater = BearLogConsoleEventUpdater::fromConsoleEventId(consoleEventId: BearGlobalStateService::getConsoleId());
-                if ($event->exitCode === 0) {
-                    $updater->setConsoleEventFinishedAt(Carbon::now());
-                } else {
-                    $updater->setConsoleEventFailedAt(Carbon::now());
-                }
-                $updater->setExecutionTimeMicroseconds((int)((microtime(as_float: true) - get_defined_constants()['LARAVEL_START']) * 1_000_000));
-                $updater->save();
-                DB::commit();
-            } catch (Throwable $t) {
-                DB::rollBack();
-                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-command-finished', severity: BearSeverityEnum::MEDIUM, exception: $t);
-            }
-        });
-
-
-        Event::listen(events: ScheduledTaskFinished::class, listener: static function ($event) {
-            try {
-                DB::beginTransaction();
-                $updater = BearLogConsoleEventUpdater::fromConsoleEventId(consoleEventId: BearGlobalStateService::getConsoleId());
-                if ($event->task->exitCode === 0) {
-                    $updater->setConsoleEventFinishedAt(Carbon::now());
-                } else {
-                    $updater->setConsoleEventFailedAt(Carbon::now());
-                    $command = $event->task->command;
-                    BearLogErrorCreator::create(
-                        message: "Scheduled task [$command] failed",
-                        namespace: 'larabear',
-                        key: 'scheduled-task-failed',
-                        severity: BearSeverityEnum::HIGH,
-                    );
-                }
-                $updater->setExecutionTimeMicroseconds((int)($event->runtime * 1_000_000));
-                $updater->save();
-                DB::commit();
-            } catch (Throwable $t) {
-                DB::rollBack();
-                BearLogErrorCreator::create(message: $t->getMessage(), namespace: 'larabear', key: 'log-console-scheduled-task-finished', severity: BearSeverityEnum::MEDIUM, exception: $t);
-            }
-        });
     }
 }
