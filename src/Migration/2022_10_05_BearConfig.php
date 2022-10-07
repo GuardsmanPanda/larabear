e1b5d988c52a7674e273e595baf628776bda2b76
Bjørn Dons <guardsmanpanda@gmail.com>
2022-10-07 09:49:32 +0200

    fix config

--- src/Migration/2022_10_05_BearConfig.php
+++ src/Migration/2022_10_05_BearConfig.php
@@ -10,32 +10,47 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void {
         Schema::dropIfExists(table: 'bear_config');
-        Schema::create(table: 'bear_config', callback: static function (Blueprint $table): void {
-            if (BearDBService::defaultConnectionDriver() === 'pgsql') {
+        if (BearDBService::defaultConnectionDriver() === 'mysql') {
+            DB::statement("
+                    CREATE TABLE bear_config (
+                        config_key VARCHAR(255) PRIMARY KEY,
+                        config_string TEXT NULL,
+                        encrypted_config_string TEXT NULL,
+                        config_boolean TINYINT(1) NULL,
+                        config_integer BIGINT NULL,
+                        config_date DATE NULL,
+                        config_timestamp TIMESTAMP NULL,
+                        config_json JSON NULL DEFAULT (json_object()),
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    );
+            ");
+        } else {
+            Schema::create(table: 'bear_log_console_event', callback: static function (Blueprint $table): void {
                 $table->text(column: 'config_key')->primary();
-            } else {
-                $table->string(column: 'config_key')->primary();
-            }
-            $table->text(column: 'config_string')->nullable();
-            $table->text(column: 'encrypted_config_string')->nullable();
-            $table->boolean(column: 'config_boolean')->nullable();
-            $table->integer(column: 'config_integer')->nullable();
-            $table->date(column: 'config_date')->nullable();
-            $table->timestampTz(column: 'config_timestamp')->nullable();
-            if (BearDBService::defaultConnectionDriver() === 'pgsql') {
-                $table->jsonb(column: 'config_json')->default( '{}');
-            } else {
-                $table->jsonb(column: 'config_json')->default( '(json_object())');
-            }
-            $table->timestampTz(column: 'created_at')->default(DB::raw( 'CURRENT_TIMESTAMP'));
-            $table->timestampTz(column: 'updated_at')->default(DB::raw( 'CURRENT_TIMESTAMP'));
-        });
-
-        BearConfigCreator::create(config_key: 'larabear.version', config_string: '0.4.0');
-        BearConfigCreator::create(config_key: 'larabear.last_short_url_code', config_string: '1');
-        BearConfigCreator::create(config_key: 'larabear.delete_log_console_event_after_days', config_integer: 60);
-        BearConfigCreator::create(config_key: 'larabear.delete_log_response_error_after_days', config_integer: 60);
-        BearConfigCreator::create(config_key: 'larabear.delete_log_access_token_after_days', config_integer: 60);
+                $table->text(column: 'config_string')->nullable();
+                $table->text(column: 'encrypted_config_string')->nullable();
+                $table->boolean(column: 'config_boolean')->nullable();
+                $table->bigInteger(column: 'config_integer')->nullable();
+                $table->date(column: 'config_date')->nullable();
+                $table->timestampTz(column: 'config_timestamp')->nullable();
+                $table->jsonb(column: 'config_json')->default('{}');
+                $table->timestampTz(column: 'created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+                $table->timestampTz(column: 'updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            });
+        }
+        try {
+            DB::beginTransaction();
+            BearConfigCreator::create(config_key: 'larabear.version', config_string: '0.4.0');
+            BearConfigCreator::create(config_key: 'larabear.last_short_url_code', config_string: '1');
+            BearConfigCreator::create(config_key: 'larabear.delete_log_console_event_after_days', config_integer: 60);
+            BearConfigCreator::create(config_key: 'larabear.delete_log_response_error_after_days', config_integer: 60);
+            BearConfigCreator::create(config_key: 'larabear.delete_log_access_token_after_days', config_integer: 60);
+            DB::commit();
+        } catch (Throwable $t) {
+            DB::rollBack();
+            throw $t;
+        }
     }
 
     public function down(): void {
