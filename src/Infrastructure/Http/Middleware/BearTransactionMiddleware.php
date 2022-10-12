43f0cb2c8b739274d3eb7cb54414479ad438b7b7
Bjørn Dons <guardsmanpanda@gmail.com>
2022-10-12 11:55:56 +0200

    log transaction errors

--- src/Infrastructure/Http/Middleware/BearTransactionMiddleware.php
+++ src/Infrastructure/Http/Middleware/BearTransactionMiddleware.php
@@ -3,6 +3,8 @@
 namespace GuardsmanPanda\Larabear\Infrastructure\Http\Middleware;
 
 use Closure;
+use GuardsmanPanda\Larabear\Enum\BearSeverityEnum;
+use GuardsmanPanda\Larabear\Infrastructure\Error\Crud\BearLogErrorCreator;
 use GuardsmanPanda\Larabear\Infrastructure\Http\Service\Req;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -22,10 +24,22 @@ class BearTransactionMiddleware {
                 DB::commit();
             } else {
                 DB::rollBack();
+                BearLogErrorCreator::create(
+                    message: 'Transaction rolled back due to HTTP status code ' . $res->getStatusCode(),
+                    namespace: 'larabear',
+                    key: 'transaction_rolled_back',
+                    severity: BearSeverityEnum::HIGH,
+                );
             }
             return $res;
         } catch (Throwable $t) {
             DB::rollBack();
+            BearLogErrorCreator::create(
+                message: 'Transaction rolled back due to exception: ' . $t->getMessage(),
+                namespace: 'larabear',
+                key: 'transaction_rolled_back_exception',
+                severity: BearSeverityEnum::HIGH,
+            );
             throw new RuntimeException(message: "Transaction failed: [{$t->getMessage()}]", code: $t->getCode(), previous: $t);
         }
     }
