44a2b6647ec8c25acea26101612b0bee8e94280c
Bjørn Dons <guardsmanpanda@gmail.com>
2022-10-26 16:10:02 +0200

    added deleter

--- src/Infrastructure/Config/Service/BearConfigService.php
+++ src/Infrastructure/Config/Service/BearConfigService.php
@@ -37,17 +37,4 @@ class BearConfigService {
     public static function getTimestamp(string $config_key): CarbonInterface {
         return BearConfig::findOrFail(id: $config_key)->config_timestamp;
     }
-
-    public static function getDataForConfigFile(): array {
-        return array_map(callback: static fn($row) => match ($row->config_data_type) {
-            'BOOLEAN' => $row->config_boolean,
-            'INTEGER' => $row->config_integer,
-            'STRING' => $row->config_string,
-            'ENCRYPTED_STRING' => $row->encrypted_config_string,
-            'JSON' => $row->config_json,
-            'DATE' => $row->config_date,
-            'TIMESTAMP' => $row->config_timestamp,
-            default => throw new RuntimeException(message: 'Invalid config data type: ' . $row->config_data_type),
-        }, array: DB::select("SELECT * FROM bear_config"));
-    }
 }
---
+++ src/Infrastructure/Error/Crud/BearLogResponseErrorDeleter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GuardsmanPanda\Larabear\Infrastructure\Error\Crud;
+
+use Illuminate\Support\Facades\DB;
+
+class BearLogResponseErrorDeleter {
+    public static function delete(
+        int $id = null,
+        int $response_status_code = null,
+        string $request_http_path = null,
+        string $app_action_name = null,
+    ): void {
+        $filters = [];
+        if ($id !== null) {
+            $filters['id'] = $id;
+        }
+        if ($response_status_code !== null) {
+            $filters['response_status_code'] = $response_status_code;
+        }
+        if ($request_http_path !== null) {
+            $filters['request_http_path'] = $request_http_path;
+        }
+        if ($app_action_name !== null) {
+            $filters['app_action_name'] = $app_action_name;
+        }
+        DB::delete("DELETE FROM bear_log_response_error WHERE " . implode(' AND ', array_map(static fn ($key) => "$key = ?", array_keys($filters))), array_values($filters));
+    }
+}
