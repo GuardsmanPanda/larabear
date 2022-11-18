58fcab10d590a04ff52d53819e740399cec98a2f
Bjørn Dons <guardsmanpanda@gmail.com>
2022-11-18 22:33:39 +0100

    fix

--- src/Infrastructure/Http/Service/Req.php
+++ src/Infrastructure/Http/Service/Req.php
@@ -130,7 +130,7 @@ class Req {
     }
 
     public static function allQueryData(): array {
-        return self::request()->query() ?? [];
+        return self::$r?->query() ?? [];
     }
 
     public static function content(): string {
