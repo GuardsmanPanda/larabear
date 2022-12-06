792e1609c03600afe49f441db640baa7df1b8dbf
Bjørn Dons <guardsmanpanda@gmail.com>
2022-12-06 13:23:41 +0100

    fix layout

--- src/Web/Www/Shared/View/form/month.blade.php
+++ src/Web/Www/Shared/View/form/month.blade.php
@@ -8,7 +8,7 @@
         <label for="{{ $id }}" class="block text-sm font-medium text-gray-700">{{ $label }}</label>
     @endif
     <div class="mt-0.5">
-        <input type="date" name="{{ $id }}" id="{{ $id }}" value="{{ $slot }}" aria-describedby="{{ $label }}" {{ $attributes->whereDoesntStartWith('class') }}
+        <input type="month" name="{{ $id }}" id="{{ $id }}" value="{{ $slot }}" aria-describedby="{{ $label }}" {{ $attributes->whereDoesntStartWith('class') }}
         class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded placeholder-gray-400">
     </div>
 </div>
