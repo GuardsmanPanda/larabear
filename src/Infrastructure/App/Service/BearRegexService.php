da76250a6ea92100b56a2cd24b72f62b8355187c
Bjørn Dons <guardsmanpanda@gmail.com>
2022-12-14 10:24:20 +0100

    fix supertrim

--- src/Infrastructure/App/Service/BearRegexService.php
+++ src/Infrastructure/App/Service/BearRegexService.php
@@ -11,7 +11,8 @@ class BearRegexService {
         return $matches[1] ?? ($nullIfNoMatch ? null : throw new RuntimeException(message: "No match found for regex: $regex, subject: $subject"));
     }
 
-    public static function superTrim(string $string): string {
-        return preg_replace(pattern: '/\s+/', replacement: ' ', subject: trim(string: $string)) ?? Throw new RuntimeException(message: "Failed to superTrim string: [$string]");
+    public static function superTrim(string|null $string): string|null {
+        $res = $string === null ? null : preg_replace(pattern: '/\s+/', replacement: ' ', subject: trim(string: $string));
+        return empty($res) || !is_string($res) ? null : $res;
     }
 }
