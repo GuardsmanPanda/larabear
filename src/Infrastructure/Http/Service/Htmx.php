3523620f23a7acaecf4ece32a0b6669699bed4f5
Bjørn Dons <guardsmanpanda@gmail.com>
2022-12-19 21:27:24 +0100

    added redirect to htmx class.

--- src/Infrastructure/Http/Service/Htmx.php
+++ src/Infrastructure/Http/Service/Htmx.php
@@ -2,8 +2,17 @@
 
 namespace GuardsmanPanda\Larabear\Infrastructure\Http\Service;
 
+
+use Symfony\Component\HttpFoundation\Response;
+
 class Htmx {
-    public static function refresh(): void {
+    public static function refresh(): Response {
         Resp::header(key: 'HX-Refresh', value: 'true');
+        return new Response();
+    }
+
+    public static function redirect(string $url): Response {
+        Resp::header(key: 'HX-Redirect', value: $url);
+        return new Response();
     }
 }
