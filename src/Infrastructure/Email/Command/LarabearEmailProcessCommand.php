e7f9bddb3d5ba982f28e0c778cc9334bda19e0b1
Bjørn Dons <guardsmanpanda@gmail.com>
2022-12-21 10:00:50 +0100

    email process command

--- src/Infrastructure/Email/Command/LarabearEmailProcessCommand.php
+++ src/Infrastructure/Email/Command/LarabearEmailProcessCommand.php
@@ -9,12 +9,12 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
-class EmailProcessCommand extends Command {
-    protected $signature = 'bear:email-process';
+class LarabearEmailProcessCommand extends Command {
+    protected $signature = 'larabear:email-process';
     protected $description = 'Process emails';
 
     public function handle(): void {
-        $emails = BearEmail::fromQuery(query: "SELECT * FROM email WHERE email_sent_at IS NULL");
+        $emails = BearEmail::fromQuery(query: "SELECT * FROM email WHERE email_sent_at IS NULL AND email_error_code IS NULL ORDER BY id LIMIT 30");
         foreach ($emails as $email) {
             try {
                 DB::beginTransaction();
