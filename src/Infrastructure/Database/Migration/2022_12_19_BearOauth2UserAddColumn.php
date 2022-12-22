dc3559316def55a9f0ba1bca60597f0b19ebb595
Bjørn Dons <guardsmanpanda@gmail.com>
2022-12-22 13:55:19 +0100

    remove oauth2 user data json

--- src/Infrastructure/Database/Migration/2022_12_19_BearOauth2UserAddColumn.php
+++ src/Infrastructure/Database/Migration/2022_12_19_BearOauth2UserAddColumn.php
@@ -8,15 +8,8 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
-        Schema::table('bear_oauth2_user', static function (Blueprint $table) {
-            if (BearDatabaseService::defaultConnectionDriver() === 'pgsql') {
-                $table->jsonb(column: 'oauth2_user_data_json')->default(DB::raw("'{}'::jsonb"));
-            } else {
-                $table->jsonb(column: 'oauth2_user_data_json')->default(DB::raw('(json_object())'));
-            }
-            Schema::table('bear_user', static function (Blueprint $table) {
-                $table->dropColumn('user_metadata_json');
-            });
+        Schema::table('bear_user', static function (Blueprint $table) {
+            $table->dropColumn('user_metadata_json');
         });
         Schema::table('bear_user', static function (Blueprint $table) {
             if (BearDatabaseService::defaultConnectionDriver() === 'pgsql') {
@@ -28,8 +21,5 @@ return new class extends Migration {
     }
 
     public function down(): void {
-        Schema::table('bear_oauth2_user', static function (Blueprint $table) {
-            $table->dropColumn('oauth2_user_data_json');
-        });
     }
 };
