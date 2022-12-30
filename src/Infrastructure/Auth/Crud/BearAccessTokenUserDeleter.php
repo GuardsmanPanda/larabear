abb03b7ef24d8d07c6ba190cb0f6bcc6a8ddb83e
Bjørn Dons <guardsmanpanda@gmail.com>
2022-12-30 01:34:58 +0100

    add delete currently used token.

--- src/Infrastructure/Auth/Crud/BearAccessTokenUserDeleter.php
+++ src/Infrastructure/Auth/Crud/BearAccessTokenUserDeleter.php
@@ -5,20 +5,22 @@ namespace GuardsmanPanda\Larabear\Infrastructure\Auth\Crud;
 use GuardsmanPanda\Larabear\Infrastructure\Auth\Model\BearAccessTokenUser;
 use GuardsmanPanda\Larabear\Infrastructure\Auth\Model\BearUser;
 use GuardsmanPanda\Larabear\Infrastructure\Database\Service\BearDatabaseService;
+use GuardsmanPanda\Larabear\Infrastructure\Http\Service\Req;
 
 class BearAccessTokenUserDeleter {
     public static function delete(BearAccessTokenUser $model): void {
-        BearDatabaseService::mustBeInTransaction();
-        BearDatabaseService::mustBeProperHttpMethod(verbs: ['DELETE']);
         $model->delete();
     }
 
     public static function deleteAllTokensForUser(BearUser $user): void {
-        BearDatabaseService::mustBeInTransaction();
         BearAccessTokenUser::where('user_id', $user->id)->delete();
     }
 
     public static function deleteFromHashedToken(string $hashed_token): void {
         self::delete(model: BearAccessTokenUser::findOrFail(id: $hashed_token));
+    }
+
+    public static function deleteCurrentlyUsedToken(): void {
+        self::deleteFromHashedToken(hashed_token: hash(algo: 'xxh128', data: Req::request()->bearerToken()));
     }
 }
