95f3f59b0b59a6bba6ded47fb7d54fb6d0d2e9d8
Bjørn Dons <guardsmanpanda@gmail.com>
2023-01-19 16:02:39 +0100

    added basic auth

--- src/Infrastructure/Database/Migration/2023_01_19_BearCreateConfig.php
+++ src/Infrastructure/Database/Migration/2023_01_19_BearCreateConfig.php
@@ -7,55 +7,62 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void {
-        DB::delete("DELETE FROM bear_config WHERE config_key LIKE 'larabear%'");
-        BearConfigCreator::create(
-            config_key: 'larabear::last-unique-short-code',
-            config_description: 'Used for generating unique short codes. (BearShortCodeService.php)',
-            config_data_type: BearConfigType::STRING,
-            config_string: '1'
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::path-to-redirect-if-not-logged-in',
-            config_description: 'Only used in session auth, if a user must be authenticated to see a route, but the user is not, then the user will be redirected to this path',
-            config_data_type: BearConfigType::STRING,
-            config_string: '/login'
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::path-to-redirect-after-login',
-            config_description: 'Used when authenticating through bear-auth, after a user logs in, they will be redirected to this path',
-            config_data_type: BearConfigType::STRING,
-            config_string: '/dashboard'
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::oauth2-create-user-if-not-exists',
-            config_description: 'If a user logs in via oauth2, but the user does not exist in the database, then create the user',
-            config_data_type: BearConfigType::BOOLEAN,
-            config_boolean: true
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::log-console-event-store-for-days',
-            config_description: 'How many days to store console errors.',
-            config_data_type: BearConfigType::INTEGER,
-            config_integer: 90
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::log-response-error-store-for-days',
-            config_description: 'How many days to store response errors.',
-            config_data_type: BearConfigType::INTEGER,
-            config_integer: 90
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::log-access-token-store-for-days',
-            config_description: 'How many days to store access token logs.',
-            config_data_type: BearConfigType::INTEGER,
-            config_integer: 90
-        );
-        BearConfigCreator::create(
-            config_key: 'larabear::log-database-change-store-for-days',
-            config_description: 'How many days to store database change logs.',
-            config_data_type: BearConfigType::INTEGER,
-            config_integer: 9000
-        );
+        try {
+            DB::beginTransaction();
+            DB::delete("DELETE FROM bear_config WHERE config_key LIKE 'larabear%'");
+            BearConfigCreator::create(
+                config_key: 'larabear::last-unique-short-code',
+                config_description: 'Used for generating unique short codes. (BearShortCodeService.php)',
+                config_data_type: BearConfigType::STRING,
+                config_string: '1'
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::path-to-redirect-if-not-logged-in',
+                config_description: 'Only used in session auth, if a user must be authenticated to see a route, but the user is not, then the user will be redirected to this path',
+                config_data_type: BearConfigType::STRING,
+                config_string: '/login'
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::path-to-redirect-after-login',
+                config_description: 'Used when authenticating through bear-auth, after a user logs in, they will be redirected to this path',
+                config_data_type: BearConfigType::STRING,
+                config_string: '/dashboard'
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::oauth2-create-user-if-not-exists',
+                config_description: 'If a user logs in via oauth2, but the user does not exist in the database, then create the user',
+                config_data_type: BearConfigType::BOOLEAN,
+                config_boolean: true
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::log-console-event-store-for-days',
+                config_description: 'How many days to store console errors.',
+                config_data_type: BearConfigType::INTEGER,
+                config_integer: 90
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::log-response-error-store-for-days',
+                config_description: 'How many days to store response errors.',
+                config_data_type: BearConfigType::INTEGER,
+                config_integer: 90
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::log-access-token-store-for-days',
+                config_description: 'How many days to store access token logs.',
+                config_data_type: BearConfigType::INTEGER,
+                config_integer: 90
+            );
+            BearConfigCreator::create(
+                config_key: 'larabear::log-database-change-store-for-days',
+                config_description: 'How many days to store database change logs.',
+                config_data_type: BearConfigType::INTEGER,
+                config_integer: 9000
+            );
+            DB::commit();
+        } catch (Throwable $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
 
     public function down(): void {
---
+++ src/Infrastructure/Http/Middleware/BearBasicAuthMiddleware.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace GuardsmanPanda\Larabear\Infrastructure\Http\Middleware;
+
+use Closure;
+use GuardsmanPanda\Larabear\Infrastructure\Auth\Crud\BearUserUpdater;
+use GuardsmanPanda\Larabear\Infrastructure\Auth\Model\BearUser;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class BearBasicAuthMiddleware {
+    public function handle(Request $request, Closure $next, string $field = 'user_email'): Response {
+        $user_info = $request->getUser();
+        if ($user_info === null) {
+            return new Response(content: 'No User Provided', status: 401, headers: ['WWW-Authenticate' => 'Basic']);
+        }
+        $user = BearUser::where(column: $field, operator: '=', value: $user_info)->sole();
+        if ($user->password === null || $request->getPassword() === null) {
+            return new Response(content: 'No Password Provided or user has no set password.', status: 401, headers: ['WWW-Authenticate' => 'Basic']);
+        }
+        if (!password_verify(password: $request->getPassword(), hash: $user->password)) {
+            return new Response(content: 'Invalid Password', status: 401, headers: ['WWW-Authenticate' => 'Basic']);
+        }
+        (new BearUserUpdater($user))->setLastLoginNow()->update();
+        return $next($request);
+    }
+}
