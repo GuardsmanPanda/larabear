e7df99a9077bd99ca32a682e405ea99590f24643
Bjørn Dons <guardsmanpanda@gmail.com>
2023-02-17 15:21:50 +0100

    fix headings, added h4

--- src/Web/Www/Shared/View/heading/h1.blade.php
+++ src/Web/Www/Shared/View/heading/h1.blade.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1); ?>
-<div {{ $attributes->merge(['class' => 'mb-4 pb-1 border-b border-gray-300']) }}>
+<div {{ $attributes->merge(['class' => 'mb-4 pb-0.5 border-b-2 border-gray-300']) }}>
     <div class="flex items-center">
         <div class="flex-auto">
-            <h1 class="text-2xl font-semibold text-gray-900">{{$slot}}</h1>
+            <h1 class="text-3xl font-semibold text-gray-900">{{$slot}}</h1>
             @if($subtitle !== '')
                 <div class="mt-0.5 text-sm text-gray-500">{!! $subtitle !!}</div>
             @endif
--- src/Web/Www/Shared/View/heading/h2.blade.php
+++ src/Web/Www/Shared/View/heading/h2.blade.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1); ?>
-<div {{ $attributes->merge(['class' => 'mb-3 pb-1 border-b border-gray-200']) }}>
+<div {{ $attributes->merge(['class' => 'mb-3 pb-0.5 border-b border-gray-300']) }}>
     <div class="flex items-center">
         <div class="flex-auto">
-            <h2 class="text-xl font-semibold text-gray-800">{{$slot}}</h2>
+            <h2 class="text-2xl font-semibold text-gray-800">{{$slot}}</h2>
             @if($subtitle !== '')
                 <div class="mt-0.5 text-sm text-gray-500">{!! $subtitle !!}</div>
             @endif
