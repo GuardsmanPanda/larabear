0e43c7d5da646a50e83374663e1d64cbfcff285d
Bjørn Dons <guardsmanpanda@gmail.com>
2023-02-27 13:14:51 +0100

    add collection service

--- src/Infrastructure/App/Service/BearCollectionService.php
+++ src/Infrastructure/App/Service/BearCollectionService.php
@@ -5,7 +5,7 @@ namespace GuardsmanPanda\Larabear\Infrastructure\App\Service;
 use Illuminate\Database\Eloquent\Collection;
 
 final class BearCollectionService {
-    public static function moveElements(Collection $from, Collection $to, callable $filter): void {
+    public static function moveItems(Collection $from, Collection $to, callable $filter): void {
         $from->each(function ($item) use ($to, $filter) {
             if ($filter($item)) {
                 $to->add($item);
