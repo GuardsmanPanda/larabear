3b5afc73acee1b0ff6db91dbe9d8faad889e2d5b
Bjørn Dons <guardsmanpanda@gmail.com>
2023-03-06 08:05:11 +0100

    updated table

--- src/Web/Www/Shared/View/table/row/divider.blade.php
+++ src/Web/Www/Shared/View/table/row/divider.blade.php
@@ -1,4 +1,4 @@
 <?php declare(strict_types=1); ?>
-<tr class="bg-gray-100 text-left">
+<tr class="bg-gray-50 text-left">
     <th colspan="5" class="px-2 py-1 text-sm font-medium text-gray-900">{{$slot}}</th>
 </tr>
