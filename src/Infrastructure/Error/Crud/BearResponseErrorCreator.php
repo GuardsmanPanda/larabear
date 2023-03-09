f75df1d9f248f82fe8388628823e3d0fbd763525
Bjørn Dons <guardsmanpanda@gmail.com>
2023-03-09 15:55:11 +0100

    log content in response error.

--- src/Infrastructure/Error/Crud/BearResponseErrorCreator.php
+++ src/Infrastructure/Error/Crud/BearResponseErrorCreator.php
@@ -16,11 +16,19 @@ final class BearResponseErrorCreator {
         try {
             $query = Req::allQueryData();
             $query_json = count($query) === 0 ? null : json_encode(value: $query, flags: JSON_THROW_ON_ERROR);
+
+            $requestContent = Req::content();
+            if (strlen($requestContent) > 1000000) {
+                $requestContent = "Request content too large to log [length: " . strlen($requestContent) . "]";
+            } else if ($requestContent === '') {
+                $requestContent = null;
+            }
+
             DB::insert("
             INSERT INTO bear_error_response
-            (request_ip, user_id, request_country_code, response_status_code, request_method, request_path, request_query_json, request_hostname, app_action_name, response_body, request_id)
-            VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?)",
-                [Req::ip(), BearGlobalStateService::getUserId(), Req::ipCountry(), $statusCode, Req::method(), Req::path(), $query_json, Req::hostname(), Req::actionName(), $responseBody, BearGlobalStateService::getRequestId()]);
+            (request_ip, user_id, request_country_code, response_status_code, request_method, request_path, request_query_json, request_content, request_hostname, app_action_name, response_body, request_id)
+            VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?)",
+                [Req::ip(), BearGlobalStateService::getUserId(), Req::ipCountry(), $statusCode, Req::method(), Req::path(), $query_json, $requestContent, Req::hostname(), Req::actionName(), $responseBody, BearGlobalStateService::getRequestId()]);
         } catch (Throwable $e) {
             Log::error(message: 'Failed log error response: ' . $e->getMessage());
         }
