0e5bc0fb4692f6d72958478432c14a4ae60f8fa0
Bjørn Dons <guardsmanpanda@gmail.com>
2023-04-11 13:20:23 +0200

    add file migration.

---
+++ src/Infrastructure/Database/Migration/2023_04_11_BearFile.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+use GuardsmanPanda\Larabear\Infrastructure\Database\Service\BearDatabaseService;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create(table: 'bear_file', callback: static function (Blueprint $table): void {
+            if (BearDatabaseService::defaultConnectionDriver() === 'pgsql') {
+                $table->uuid(column: 'id')->primary()->default(DB::raw('gen_random_uuid()'));
+                $table->text(column: 'original_file_name');
+                $table->text(column: 'mime_type');
+                $table->text(column: 'local_file_path')->nullable();
+                $table->text(column: 'cloud_file_path')->nullable();
+                $table->jsonb(column: 'file_data_json')->default(DB::raw("'{}'::jsonb"));
+            } else {
+                $table->uuid(column: 'id')->primary();
+                $table->string(column: 'original_file_name');
+                $table->string(column: 'mime_type');
+                $table->string(column: 'local_file_path')->nullable();
+                $table->string(column: 'cloud_file_path')->nullable();
+                $table->jsonb(column: 'file_data_json')->default(DB::raw('(json_object())'));
+            }
+            $table->uuid(column: 'created_by_user_id')->nullable();
+            $table->timestampTz(column: 'created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestampTz(column: 'updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('created_by_user_id')->references('id')->on('bear_user');
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists(table: 'bear_file');
+    }
+};
