26db9fa6e2ffc183f08be6cd23d5c5e8da037375
Bjørn Dons <guardsmanpanda@gmail.com>
2023-04-27 14:46:27 +0200

    fix endpoints for monitoring.

--- src/Web/Api/Monitoring/routes.php
+++ src/Web/Api/Monitoring/routes.php
@@ -4,8 +4,8 @@ use GuardsmanPanda\Larabear\Web\Api\Monitoring\Controller\LarabearMonitoringApiE
 use GuardsmanPanda\Larabear\Web\Api\Monitoring\Controller\LarabearMonitoringApiQueueController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(uri: 'failed-jobs-count', action: [LarabearMonitoringApiQueueController::class, 'failedJobsCount']);
-Route::get(uri: 'stale-jobs-count', action: [LarabearMonitoringApiQueueController::class, 'staleJobsCount']);
+Route::get(uri: 'no-failed-jobs', action: [LarabearMonitoringApiQueueController::class, 'failedJobsCount']);
+Route::get(uri: 'no-stale-jobs', action: [LarabearMonitoringApiQueueController::class, 'staleJobsCount']);
 
-Route::get(uri: 'soft-delete-errors', action: [LarabearMonitoringApiErrorController::class, 'softDeleteErrors']);
-Route::get(uri: 'severe-errors', action: [LarabearMonitoringApiErrorController::class, 'severeErrors']);
+Route::get(uri: 'no-soft-delete-errors', action: [LarabearMonitoringApiErrorController::class, 'softDeleteErrors']);
+Route::get(uri: 'no-severe-errors', action: [LarabearMonitoringApiErrorController::class, 'severeErrors']);
