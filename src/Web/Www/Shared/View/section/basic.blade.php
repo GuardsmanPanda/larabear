b509792a0ba47b6cb64ce50e812ccc58c2e14d14
Bjørn Dons <guardsmanpanda@gmail.com>
2023-04-28 12:21:13 +0200

    small update

--- src/Web/Www/Shared/View/section/basic.blade.php
+++ src/Web/Www/Shared/View/section/basic.blade.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1); ?>
 <div {{ $attributes->merge(['class' => $classes]) }}>
     <div class="{{$headerClasses}}">
-        <h3 class="font-medium">{{$title}}</h3>
+        <h3 class="font-semibold">{{$title}}</h3>
     </div>
     {{$slot}}
 </div>
