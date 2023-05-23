fb4bdde36d59f3ef2bbe075a85de704a051d0de4
Bjørn Dons <guardsmanpanda@gmail.com>
2023-05-23 14:09:56 +0200

    fix migration

--- src/Infrastructure/Database/Migration/2023_03_21_BearDropSeverityTable.php
+++ src/Infrastructure/Database/Migration/2023_03_21_BearDropSeverityTable.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void {
         Schema::dropIfExists(table: 'bear_error');
+        Schema::dropIfExists(table: 'bear_log_error');
         Schema::dropIfExists(table: 'bear_severity');
     }
 
