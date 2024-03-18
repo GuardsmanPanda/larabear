004b0f2a32285b08f92f5738b3de9bc9352faff0
GuardsmanPanda <guardsmanpanda@gmail.com>
2024-03-18 19:43:22 +0000

    allow omit on log change.

--- src/Integration/ExternalApi/Enum/BearExternalApiTypeEnum.php
+++ src/Integration/ExternalApi/Enum/BearExternalApiTypeEnum.php
@@ -5,6 +5,7 @@ namespace GuardsmanPanda\Larabear\Integration\ExternalApi\Enum;
 enum  BearExternalApiTypeEnum: string {
     case BASIC_AUTH = 'BASIC_AUTH';
     case BEARER_TOKEN = 'BEARER_TOKEN';
+    case NO_AUTH = 'NO_AUTH';
     case OAUTH2 = 'OAUTH2';
     case OAUTH2_CLIENT = 'OAUTH2_CLIENT';
     case X_API_KEY = 'X_API_KEY';
