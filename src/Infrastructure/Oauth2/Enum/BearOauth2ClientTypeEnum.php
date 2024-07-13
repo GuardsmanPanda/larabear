a8a90a856361c1d28f88e5c270938974f081e4e1
GuardsmanPanda <guardsmanpanda@gmail.com>
2024-07-13 23:11:02 +0100

    Improved oauth2

--- src/Infrastructure/Oauth2/Enum/BearOauth2ClientTypeEnum.php
+++ src/Infrastructure/Oauth2/Enum/BearOauth2ClientTypeEnum.php
@@ -8,4 +8,24 @@ enum BearOauth2ClientTypeEnum: string {
     case MICROSOFT = 'MICROSOFT';
     case OTHER = 'OTHER';
     case TWITCH = 'TWITCH';
+
+    public function authorizeUri(): string {
+        return match ($this) {
+            self::HELP_SCOUT => 'https://secure.helpscout.net/authentication/authorizeClientApplication',
+            self::GOOGLE => 'https://accounts.google.com/o/oauth2/auth',
+            self::MICROSOFT => 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+            self::OTHER => 'https://example.com/oauth2/authorize',
+            self::TWITCH => 'https://id.twitch.tv/oauth2/authorize',
+        };
+    }
+
+    public function tokenUri(): string {
+        return match ($this) {
+            self::HELP_SCOUT => 'https://secure.helpscout.net/authentication/token',
+            self::GOOGLE => 'https://accounts.google.com/o/oauth2/token',
+            self::MICROSOFT => 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+            self::OTHER => 'https://example.com/oauth2/token',
+            self::TWITCH => 'https://id.twitch.tv/oauth2/token',
+        };
+    }
 }
