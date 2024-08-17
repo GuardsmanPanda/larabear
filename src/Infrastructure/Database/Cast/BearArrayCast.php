31fba9448f8b3f446ce30e5755335292662bf797
GuardsmanPanda <guardsmanpanda@gmail.com>
2024-08-17 16:40:43 +0100

    Work on bear array cast.

---
+++ src/Infrastructure/Database/Cast/BearArrayCast.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace GuardsmanPanda\Larabear\Infrastructure\Database\Cast;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @implements CastsAttributes<string, array>
+ */
+final class BearArrayCast implements CastsAttributes {
+    public function get(Model $model, string $key, mixed $value, array $attributes) {
+        // TODO: Implement get() method.
+    }
+
+    public function set(Model $model, string $key, mixed $value, array $attributes) {
+        // TODO: Implement set() method.
+    }
+}
