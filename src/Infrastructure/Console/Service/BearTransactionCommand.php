4ae1fdc8fb4c511fbfc38f5f3de5b815520beeb3
GuardsmanPanda <guardsmanpanda@gmail.com>
2024-09-28 11:42:38 +0100

    ix typing of option.

--- src/Infrastructure/Console/Service/BearTransactionCommand.php
+++ src/Infrastructure/Console/Service/BearTransactionCommand.php
@@ -4,6 +4,8 @@ namespace GuardsmanPanda\Larabear\Infrastructure\Console\Service;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use InvalidArgumentException;
+use Override;
 use Throwable;
 
 abstract class BearTransactionCommand extends Command {
@@ -19,4 +21,15 @@ abstract class BearTransactionCommand extends Command {
     }
 
     abstract protected function handleInTransaction(): void;
+
+    #[Override] public final function option($key = null): bool {
+        if ($key === null) {
+            throw new InvalidArgumentException(message: "Option key must not be null");
+        }
+        $parent = parent::option($key);
+        if (!is_bool($parent)) {
+            throw new InvalidArgumentException(message: "Option $key did not return a boolean");
+        }
+        return $parent;
+    }
 }
